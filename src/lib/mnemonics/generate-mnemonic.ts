14db6d887e9a37fe18b023a99f74f61c9c4f782f
Nic Dominguez <NicDominguez@users.noreply.github.com>
2023-07-27 20:28:37 -0700

    sets mnemonic status based threshold

--- src/lib/mnemonics/generate-mnemonic.ts
+++ src/lib/mnemonics/generate-mnemonic.ts
@@ -1,13 +1,13 @@
 import { faker } from '@faker-js/faker';
 
 import dataOption from '../../data/options';
-import { shuffle } from '../../utils';
-import type { MnemonicOptions, Mnemonic, Status } from '../../types/index';
+import { shuffle, evaluateStatus } from '../../utils';
+import type { MnemonicOptions, Mnemonic } from '../../types/index';
 
 export const generateMnemonic = (options: MnemonicOptions): Mnemonic => {
   const max = options?.thresholdMax || 110;
   const min = options?.thresholdMin || 0;
-  const deviation = options?.deviation || 20;
+  const deviation = options?.deviation || 10;
   const precision = options?.precision || 0.1;
   const subsystem = options.subsystem || shuffle(dataOption.subsystems);
   const childSubsystem =
@@ -18,20 +18,22 @@ export const generateMnemonic = (options: MnemonicOptions): Mnemonic => {
   const unit = shuffle(dataOption.units);
   const assembly = unit === 'Volts' ? 'Voltage Monitor' : `Heater Switch Power`;
   const measurement = `${childSubsystem} ${childSubSystemNum} ${assembly}`;
+  const currentValue = faker.number.float({
+    max: max + deviation,
+    min: min - deviation,
+    precision,
+  });
+  const status = evaluateStatus(currentValue, min, max);
 
   return {
     id: faker.string.uuid(),
     type: 'mnemonic',
     mnemonicId: faker.string.alpha({ length: 7, casing: 'upper' }),
-    status: shuffle<Status>(dataOption.statuses),
+    status,
     unit: unit,
     thresholdMax: max,
     thresholdMin: min,
-    currentValue: faker.number.float({
-      max: max + deviation,
-      min: min - deviation,
-      precision,
-    }),
+    currentValue,
     subsystem,
     childSubsystem,
     assemblyDevice,
